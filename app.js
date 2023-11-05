71d892b6786cb8070d1c4902244eefe9d9a4546d
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-11-06 00:39:37 +0500

    complete logic

--- app.js
+++ app.js
@@ -1,72 +1,27 @@
-// function check() {
-    //     var value = textArea.value;
-    //     var lines = [];
-//     var currentLine = '';
-    
-//     for (var i = 0; i < value.length; i++) {
-//       currentLine += value[i];
-//       if (value[i] === '\n' || i === value.length - 1) {
-    //         // If a newline character is found or we're at the end of the value, consider it a line
-    //         if (currentLine.includes("?")) {
-//           console.log("Line with a question mark: " + currentLine);
-//           // You can save the line to an array or perform other actions here
-//         }
-//         // currentLine = ''; // Reset the currentLine variable
-//       }
-//     }
-//   }
-
-
-    // var textArea = document.getElementById("textArea");
-
-    // function check(){
-    //     var value = textArea.value;
-    //     var linesWithQuestions = []
-    //     var currentLine = '';
-    //     for(i = 0; i < value.length;i++){
-    //         currentLine += value[i]
-    //         // console.log(value[i])
-    //         if(value[i]=== "\n"){
-    //             if(currentLine.includes("?")){
-    //                 linesWithQuestions.push(currentLine)
-                    
-    //             }
-    //             currentLine = ""
-    //         }
-
-
-    //     }
-    //     return linesWithQuestions;
-    // }
-
-    // var linesWithQuestions = check();
-    // console.log(linesWithQuestions);
-
-
-
-    var textArea = document.getElementById("textArea");
-
-function check() {
-    var value = textArea.value;
-    var linesWithQuestions = []; // Create an array to store lines with questions
-    var currentLine = '';
-    
-    for (var i = 0; i < value.length; i++) {
-        currentLine += value[i];
-        if (value[i] === "\n") {
-            if (currentLine.includes("?")) {
-                linesWithQuestions.push(currentLine); // Add the line to the array
-            }
-            currentLine = "";
+var textArea = document.getElementById("textArea");
+var obj = {};
+var question = [];
+function submit() {
+  var value = textArea.value.split("\n");
+  var count = 1;
+  for (var i = 0; i < value.length; i++) {
+    if (value[i] === "") {
+      question.push(obj);
+      obj = {};
+      count = 1
+    } else {
+      if (value[i].indexOf("?") !== -1) {
+        obj.question = value[i];
+      } else {
+        if (value[i].indexOf("*") !== -1) {
+          obj.correct = value[i].slice(0, -1);
+          obj["opption" + count] = value[i].slice(0, -1);
+        } else {
+          obj["opption" + count] = value[i];
         }
+        count++;
+      }
     }
-    
-    return linesWithQuestions; // Return the array of lines with questions
+  }
+  console.log(question);
 }
-
-var linesWithQuestions = check(); // Call the function and store the result
-
-// Access the lines with questions outside the function
-console.log(linesWithQuestions);
-
-
